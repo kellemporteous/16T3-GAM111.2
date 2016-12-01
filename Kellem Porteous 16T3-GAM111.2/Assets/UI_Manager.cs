9b44a0841e6134eb684c098b4707ef406eca8c00
SAE\kporteous <1005781@student.sae.edu.au>
2016-12-01 11:35:45 +1000

    Started working on Tile management

---
+++ Kellem Porteous 16T3-GAM111.2/Assets/UI_Manager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class UI_Manager : MonoBehaviour {
+
+    public EventSystem levelEventSystem;
+    private Tile_Attach react;
+
+	// Use this for initialization
+	void Start ()
+    {
+        react = GetComponent<Tile_Attach>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ReactToTiles();
+
+    }
+
+    void ReactToTiles()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit HitResults;
+            if (Physics.Raycast(ray, out HitResults))
+            {
+                GameObject HitObject = HitResults.collider.gameObject;
+
+                react = HitObject.GetComponent<Tile_Attach>();
+                if (react != null)
+                {
+                    react.ReactToMouseClick();
+                }
+            }
+        }
+    }
+}
